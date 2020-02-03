2a76730b0ffb12e5a64802157d39a8b0c6fff873
rafanleme <rafanleme@gmail.com>
2020-02-03 10:32:03 -0300

    Primeiro commit

---
+++ config/conexao-db.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql')
+
+const conexao = mysql.createConnection({
+    host: '52.4.2.59',
+    port: 3306,
+    user: 'profrafa',
+    password: 'bcd127',
+    database: 'brabank'
+})
+
+module.exports = conexao
+
